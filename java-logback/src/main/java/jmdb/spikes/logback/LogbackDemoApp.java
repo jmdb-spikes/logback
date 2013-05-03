4548a1318ae16f0c843c8fed931c6dc120f71fcc
Jim Barritt <jim@planet-ix.com>
2013-05-04 00:10:51 +0300

    [jmdb] Nicely formatted log messages

--- java-logback/src/main/java/jmdb/spikes/logback/LogbackDemoApp.java
+++ java-logback/src/main/java/jmdb/spikes/logback/LogbackDemoApp.java
@@ -16,17 +16,23 @@ public class LogbackDemoApp {
     /**
      * Possible patterns
      *
+     * The logger{size} is the TOTAL width of the logger name, so with shorten things starting with left most package
+     *
      * %date{ISO8601}
      * [%-10thread]
+     * @see http://logback.qos.ch/manual/layouts.html
+     * @see http://en.wikipedia.org/wiki/ISO_8601
+     * @see http://docs.oracle.com/javase/1.4.2/docs/api/java/text/SimpleDateFormat.html
      */
     public static void main(String[] args) {
-        initialiseConsoleLogging(Level.TRACE, "[%date{yyyy-mm-dd hh:MM:ss.SSS z (Z)}] %-6level %-35class - %message%n");
+        initialiseConsoleLogging(Level.TRACE, "[%date{yyyy-mm-dd hh:MM:ss.SSS z Z]} %-6level %-35logger{35} - %message%n");
 
         log.trace("VERY LOW LEVEL");
         log.debug("Some debug...");
         log.info("Hello [{}]", "jim");
         log.warn("Ooh a warning...");
         log.error("OMG! AN ERROR!!!");
+        log.error("AN Error with stack trace!", new IllegalAccessException("BOOM!"));
     }
 
     private static void initialiseConsoleLogging(Level level, String pattern) {
